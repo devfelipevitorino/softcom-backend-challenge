00921d46456a2eaa97930ae6312bce62e9d00525
devfelipevitorino <devfelipevitorino@gmail.com>
2025-08-09 14:23:55 -0300

    rotas para crud de item

--- app/routes/item.routes.js
+++ app/routes/item.routes.js
@@ -1,12 +1,24 @@
 import express from "express";
-import { itensList } from "../controllers/item.controller.js";
+import { 
+    itensList,
+    itemFindById,
+    itemCreate,
+    itemUpdate,
+    itemDelete
+} from "../controllers/item.controller.js";
 
-import {
-    verifyToken
-} from "../middlewares/authJwt.js";
+import { verifyToken } from "../middlewares/authJwt.js";
 
 const router = express.Router(); 
 
 router.get("/user/itens", [verifyToken], itensList);
+
+router.get("/user/itens/:id", [verifyToken], itemFindById);
+
+router.post("/user/itens", [verifyToken], itemCreate);
+
+router.put("/user/itens/:id", [verifyToken], itemUpdate);
+
+router.delete("/user/itens/:id", [verifyToken], itemDelete);
 
 export default router;
